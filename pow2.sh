bf5a2731ace5fa23ad9caad9b30e2bd51a60eff4
shashank <shashank.w1998@gmail.com>
2021-02-17 18:01:06 +0530

    primefact added

--- pow2.sh
+++ pow2.sh
@@ -77,3 +77,14 @@ done
 echo "Factorial of " $Num "=" $fac
  
 
+#! /bin/bash
+read -p "Enter number : " num
+for ((i=2;i<=$num;i++))
+do
+   while [ $(($num%$i)) -eq 0 ]
+   do
+      echo $i
+      num=$(($num/$i))
+   done
+done
+
