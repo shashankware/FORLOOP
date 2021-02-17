f4636d3237d287f8049986f6d7d97eaabae4cabf
shashank <shashank.w1998@gmail.com>
2021-02-17 17:36:11 +0530

    prime2 added

--- pow2.sh
+++ pow2.sh
@@ -38,3 +38,31 @@ r=`prime $number`
 echo "$r"
 
 
+#!/bin/bash
+ read -p "Enter Number : " N
+for ((j=1;j<=N;j++))
+do
+   if [ $j -eq "1" ] || [ $j -eq "2" ]
+   then
+       echo "Prime Number" $j
+   else
+      for ((i=2;i<j;i++))
+      do
+         temp=$(($j%$i))
+         if [ $temp -eq "0" ]
+         then
+            flag=1
+            break
+         else
+            flag=0
+         fi
+      done
+      if [ $flag -eq "1" ]
+      then
+         echo "Not Prime Number" $j
+      else
+         echo "Prime Number" $j
+      fi
+   fi
+done
+
